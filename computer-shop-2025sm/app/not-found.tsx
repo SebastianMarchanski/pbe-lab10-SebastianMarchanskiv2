d13659ff1441e03a0443da2a23aa285522116b08
SebastianMarchanski <165995138+SebastianMarchanski@users.noreply.github.com>
2025-11-18 16:04:13 +0000

    Task 9.1 - add not-found

--- computer-shop-2025sm/app/not-found.tsx
+++ computer-shop-2025sm/app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound(){
         <main className="not-found">
             <h1>Nie znaleziono strony</h1>
             <p>Srona, której szukasz, nie istnieje.</p>
-            <p>Sprawdź adres URL lub wróc na stronę główną.</p>
+            <p>Sprawdź adres URL lub wróc na stronę główną</p>
         </main>
     )
 }
