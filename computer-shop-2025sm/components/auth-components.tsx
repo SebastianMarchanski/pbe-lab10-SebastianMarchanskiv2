23d2f4b441713cc32f88bc884cb873500572793f
SebastianMarchanski <sebastian.marchanski@student.pk.edu.pl>
2026-01-06 17:15:22 +0100

    Fix GitHub OAuth callback URL and login redirect to /basket

--- computer-shop-2025sm/components/auth-components.tsx
+++ computer-shop-2025sm/components/auth-components.tsx
@@ -22,7 +22,7 @@ export function LogoutButton() {
       onClick={() => signOut({ callbackUrl: "/" })}
       className="px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-700"
     >
-      Wyloguj się
+      Wyloguj się z githuba
     </button>
   )
 }
