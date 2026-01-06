f515aa785e913ee4594e2d525c7ea64b5906aa33
SebastianMarchanski <sebastian.marchanski@student.pk.edu.pl>
2026-01-06 16:32:46 +0100

    Fix Prisma Vercel deployment

--- computer-shop-2025sm/next.config.ts
+++ computer-shop-2025sm/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/*": ["./node_modules/.prisma/client/**/*"],
+  },
 };
 
 export default nextConfig;
